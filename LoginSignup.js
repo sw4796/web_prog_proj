6ad5c62abf2c9931eea1bde6d369b1f860c55ccc
kimbap1001 <117625125+kimbap1001@users.noreply.github.com>
2022-12-05 00:07:35 +0900

    Add files via upload
    로그인회원가입창 + 회원가입 db로 옮기는 창
    회원가입 시 db에 저장, 비밀번호 찾기, 로그인 정보 맞을 시 메인페이지로 이동까지 구현
    로그인 성공 시 세션 생성은 아직 안함

--- LoginSignup.js
+++ LoginSignup.js
@@ -11,3 +11,39 @@ window.onload=function(){
         container.classList.remove("right-panel-active");
     });
 }
+
+function signupCheck()
+{
+	const message2=document.getElementById("message2");
+	let id=document.getElementById("id").value;
+	let name=document.getElementById("name").value;
+	let pw1=document.getElementById("pw1").value;
+	let pw2=document.getElementById("pw2").value;
+	let Qans=document.getElementById("Qans").value;
+	let idcheck=document.getElementById("idcheck").class;
+	//message2.innerHTML=id+","+name+","+pw1+","+Qans+","+idcheck;
+	if((id=="")||(name=="")||(pw1=="")||(pw1!=pw2)||(Qans=="")||(idcheck=="uncheck")===true)
+	{
+		alert("모든 정보를 입력해야 합니다.");
+	}
+	else
+	{
+		document.getElementById("signup").submit();
+	}
+}
+
+function passCheck()
+{   
+    const message=document.getElementById("message");
+    let pw1=document.getElementById("pw1").value;
+	let pw2=document.getElementById("pw2").value;
+    if(pw1==pw2)
+    {
+        message.innerHTML="비밀번호가 일치합니다.";
+        
+    }
+    else
+    {
+        message.innerHTML="비밀번호가 일치하지 않습니다.";
+    }
+}
